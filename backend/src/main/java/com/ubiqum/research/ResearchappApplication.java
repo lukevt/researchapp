4fa0fa468c5ef145e1e37dd9f56be50800cd469b
lfcsvt <lfcsvt@gmail.com>
2020-09-17 10:03:52 +0100

    first commit

---
+++ backend/src/main/java/com/ubiqum/research/ResearchappApplication.java
@@ -0,0 +1,13 @@
+package com.ubiqum.research;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearchappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResearchappApplication.class, args);
+	}
+
+}
