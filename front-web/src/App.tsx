64e1dbe194c3b744b1a3d45b33eaacc5f7ba5c56
lfcsvt <lfcsvt@gmail.com>
2020-09-19 08:38:19 +0100

    home page created

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,15 @@
+import { homedir } from 'os';
 import React from 'react';
 import './App.css';
 import Header from './components/Header'
+import Home from './pages/Home'
 
 
 function App() {
   return (
     <div>
         <Header/>
+        <Home/>
     </div>
     
   );
