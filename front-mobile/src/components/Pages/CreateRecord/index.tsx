04266acc5a38c3126260ad4d8ca6db3eabb8a4e8
lfcsvt <lfcsvt@gmail.com>
2020-09-20 22:06:13 +0100

    changed base-url

--- front-mobile/src/components/Pages/CreateRecord/index.tsx
+++ front-mobile/src/components/Pages/CreateRecord/index.tsx
@@ -13,7 +13,7 @@ const placeholder = {
     label: "Select the Game",
     value: null
 }
-const BASE_URL = "http://192.168.1.70:8080"
+const BASE_URL = "https://sds1-lfcsvt.herokuapp.com"
 const mapSelectValues = (games : Game[])=> {
   return games.map(game=>({
     ...game,
